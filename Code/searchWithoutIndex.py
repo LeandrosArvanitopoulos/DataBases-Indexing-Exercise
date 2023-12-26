ebd8c524fc0c1c5908a5cfece4682fd0b809497e
LeandrosArvanitopoulos <110211344+LeandrosArvanitopoulos@users.noreply.github.com>
2023-12-26 23:35:46 +0200

    Update searchWithoutIndex.py
    Create the Index file and compare results

--- Code/searchWithoutIndex.py
+++ Code/searchWithoutIndex.py
@@ -1,13 +1,22 @@
 import sqlite3 as sl3
 import timeit
 
+times = []
 
+
+# This wrapper is used to calculate the time taken by a function to execute
 def timeit_wrapper(func):
     def wrapper(*args, **kwargs):
         start = timeit.default_timer()
         result = func(*args, **kwargs)
         end = timeit.default_timer()
-        print(f"{func.__name__} took {end - start} seconds")
+        if func.__name__ == "executeQuery":
+            print("-" * len(f"{func.__name__} took {end - start} seconds"))
+            print(f"{func.__name__} took {end - start} seconds")
+            print("-" * len(f"{func.__name__} took {end - start} seconds"))
+            times.append(end - start)
+        else:
+            print(f"{func.__name__} took {end - start} seconds")
         return result
 
     return wrapper
@@ -36,8 +45,10 @@ def executeQuery(
 
     connection.commit()
     record = cursor.fetchall()
-    print(f"The result of the {query}\n\tis\n\t", record)
-    cursor.close()
+    if record != [] and record != None:
+        print(f"The result of the {query}\n\tis\n\t", record)
+    else:
+        print(f"For the query {query}")
 
 
 @timeit_wrapper
@@ -51,6 +62,28 @@ def proccess():
     print("\n")
 
     # close connection
+    cursor.close()
+    connection.close()
+    print("SQLite connection is closed")
+
+
+@timeit_wrapper
+def proccessWithIndex():
+    # create database
+    connection, cursor = createConnection()
+    print("\n")
+
+    # execute query
+    executeQuery(
+        cursor,
+        connection,
+        query="""CREATE INDEX IF NOT EXISTS idx_airports_lat_deg ON airports(latitude_deg);""",
+    )
+    executeQuery(cursor, connection)
+    print("\n")
+
+    # close connection
+    cursor.close()
     connection.close()
     print("SQLite connection is closed")
 
@@ -61,9 +94,27 @@ if __name__ == "__main__":
     proccess()
     print("\n")
     print("Done!")
+    print("\n")
 
     print("Running with index..")
     print("\n")
-    proccess()
+    proccessWithIndex()
     print("\n")
     print("Done!")
+    print("\n")
+
+    print(
+        "Time taken to execute query without index:"
+        + " " * (50 - len("Time taken to execute query without index:")),
+        times[0],
+    )
+    print(
+        "Time taken to execute query with index: "
+        + " " * (50 - len("Time taken to execute query with index: ")),
+        times[1] + times[2],
+    )
+
+    print(
+        "The acceleration is" + " " * (50 - len("The acceleration is")),
+        times[0] / (times[1] + times[2]),
+    )
